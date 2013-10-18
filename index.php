595e5c647def0cef325691a176529006f6ec20d4
cesarhdz <mrshortcuts@gmail.com>
2013-10-18 00:10:23 -0500

    index.php added, ready to load wordpress/wp-blog-header.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** 
+ * Loads the WordPress Environment and Template
+ *
+ * Must match (defaults to wordpress)
+ * 	"extra" : {
+ * 		"wordpress-install-dir": "wp"
+ * 	}
+ */
+require('./wordpress/wp-blog-header.php');
