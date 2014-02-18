60ce9f07d1141e1b32ff2955042075008c8fe448
cesarhdz <mrshortcuts@gmail.com>
2014-02-18 12:42:31 -0600

    Configuration files for dev and prod added

---
+++ config/config.dev.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Main Wordpress Configuration for development
+ * All keys of $config array will be defined as constants after mergin with config.php file
+ */
+$config = array();
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+$config['WP_DEBUG'] = 'true';
+
+
+
+
+// Must return $config array
+return $config;
+
---
+++ config/config.prod.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Main Wordpress Configuration for production
+ * All keys of $config array will be defined as constants after mergin with config.php file
+ */
+$config = array();
+
+
+/**
+ * PHP Memory
+ */
+// $config['WP_MEMORY_LIMIT'] = '64M';
+
+// Must return $config array
+return $config;
+
